7930c8b2c1c555fa2b3fa2b837b893f8b8834b9d
Niki Shah <nikhil@anuniki.com>
2017-11-26 10:55:01 -0500

    Started work on lab 10

---
+++ Rowan/src/lab11/KeyedItem.java
@@ -0,0 +1,13 @@
+package lab11;
+
+public abstract class KeyedItem<KT extends Comparable<? super KT>> {
+	private KT searchKey;
+
+	public KeyedItem(KT key) {
+		searchKey = key;
+	} // end constructor
+
+	public KT getKey() {
+		return searchKey;
+	} // end getKey
+} // end KeyedItem
