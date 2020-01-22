3cf11e9099ab1ff6fc740a8ba487b537c1601e26
n-man13 <13nman@gmail.com>
2020-01-22 10:51:10 -0500

    Initial Coding brush up

---
+++ Rutgers/src/dataStructuresNotes/ReviewOfSyntax.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package dataStructuresNotes;
+
+/**
+ * @author nikis
+ *
+ */
+public class ReviewOfSyntax {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
