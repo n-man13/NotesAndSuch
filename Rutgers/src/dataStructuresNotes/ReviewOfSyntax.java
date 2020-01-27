50195867fedea9eef871ba9db34b8377a96f990f
n-man13 <13nman@gmail.com>
2020-01-27 11:00:33 -0500

    Started work on day 2 of class

--- Rutgers/src/dataStructuresNotes/ReviewOfSyntax.java
+++ Rutgers/src/dataStructuresNotes/ReviewOfSyntax.java
@@ -15,7 +15,8 @@ public class ReviewOfSyntax {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		
+		boolean myFlag = false;
+		System.out.println("The value of myFlag is " + myFlag);
 	}
 
 }
