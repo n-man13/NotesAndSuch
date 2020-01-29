ad3e9bbf8959bdcebf4730e763bf7b52a77dc3ee
n-man13 <13nman@gmail.com>
2020-01-29 11:12:38 -0500

    Added summation function

--- Rutgers/src/dataStructuresClassNotes/InitArray.java
+++ Rutgers/src/dataStructuresClassNotes/InitArray.java
@@ -3,13 +3,16 @@ package dataStructuresClassNotes;
 public class InitArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		int[] myArray = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
 		
 		System.out.printf("%5s%8s%n", "Index", "Value");
 		
-		for(int count = 0; count < array.length; count++)
-			System.out.printf("%5d%8d%n", count, array[count]);
+		int total = 0;
+		for(int count = 0; count < myArray.length; count++) {
+			System.out.printf("%5d%8d%n", count, myArray[count]);
+			total += myArray[count];
+		}
+		System.out.println("\nThe total is: " + total + ".");
 	}
 
 }
