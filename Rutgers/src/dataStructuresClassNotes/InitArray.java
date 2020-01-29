f169986ce86a87872aaf17a9c8ca9197fe5193b7
n-man13 <13nman@gmail.com>
2020-01-29 10:25:26 -0500

    Fixed formatting of title line

--- Rutgers/src/dataStructuresClassNotes/InitArray.java
+++ Rutgers/src/dataStructuresClassNotes/InitArray.java
@@ -6,7 +6,7 @@ public class InitArray {
 		// TODO Auto-generated method stub
 		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
 		
-		System.out.printf("%s%8s%n", "Index", "Value");
+		System.out.printf("%5s%8s%n", "Index", "Value");
 		
 		for(int count = 0; count < array.length; count++)
 			System.out.printf("%5d%8d%n", count, array[count]);
