22f47ed5aeba82ad1fdcfde405b4f96d999113b8
n-man13 <13nman@gmail.com>
2020-02-12 10:35:11 -0500

    Added small amount of work on example.java

--- Rutgers/src/dataStructuresClassNotes/Example.java
+++ Rutgers/src/dataStructuresClassNotes/Example.java
@@ -1,10 +1,17 @@
 package dataStructuresClassNotes;
 
+import java.util.Arrays;
+import java.lang.Math;
+
 public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	}
+	public static void arrIntTesting() {
+		
 	}
 
 }
