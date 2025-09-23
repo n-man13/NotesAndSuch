973848778436fe70d0c3ac1c9be0acbb310d2422
Niki Shah <email4nikhil@gmail.com>
2025-09-23 18:09:49 -0400

    rewrote adder to use structural modeling as per instructions

--- StonyBrook/ComputerArchitecture/Homework1.v
+++ StonyBrook/ComputerArchitecture/Homework1.v
@@ -2,8 +2,21 @@ module FullAdder(input a, input b, input ci, output s, output co);
     /*
     Implement a full adder using verilog
     */
-    assign s = a ^ b ^ ci;
-    assign co = (a & b) | (b & ci) | (a & ci);
+    wire tempSum;
+    wire foo;
+    wire bar;
+    wire baz;
+    wire quux;
+
+
+    xor(temp, a, b);
+    xor(s, temp, ci);
+
+    and(foo, a, b);
+    and(bar, b, ci);
+    and(baz, a, ci);
+    or(quux, foo, bar);
+    or(co, baz, quux);
 
 endmodule
 
