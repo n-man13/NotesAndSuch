5acf33646858771a4a7a277dd3e2151bf09129d0
Niki Shah <email4nikhil@gmail.com>
2025-09-24 19:54:37 -0400

    trying and failing around

--- StonyBrook/ComputerArchitecture/Homework1.v
+++ StonyBrook/ComputerArchitecture/Homework1.v
@@ -2,7 +2,7 @@ module FullAdder(input a, input b, input ci, output s, output co);
     /*
     Implement a full adder using verilog
     */
-    wire tempSum;
+    wire temp;
     wire foo;
     wire bar;
     wire baz;
@@ -20,17 +20,35 @@ module FullAdder(input a, input b, input ci, output s, output co);
 
 endmodule
 
+module FourBitAdder(input [3:0] a, input [3:0] b, output [4:0] s);
+    /*
+    implement a 4 bit adder using full adder from above
+    */
+    wire [2:0] c;
+    FullAdder first(a[0], b[0], 0, s[0], c[0]);
+    FullAdder second(a[1], b[1], c[0], s[1], c[1]);
+    FullAdder third(a[2], b[2], c[1], s[2], c[2]);
+    FullAdder fourth(a[3], b[3], c[2], s[3], s[4]);
+    
+
+
+endmodule
+
 module test;
 reg a,b,cin;
+// reg [3:0] one,two;
+// reg [4:0] sum;
 wire s,co;
+integer i;
+
 
 
 FullAdder adder(a,b,cin,s,co);
+// FourBitAdder four(one,two,sum);
 
 initial begin
-    $dumpfile("test.vcd");
-    $dumpvars(0,test);
-    a = 0; b = 0; cin = 0;
+    
+    a = 0; b = 0; cin = 0; 
     #10
     a = 0; b = 0; cin = 1;
     #10
@@ -46,6 +64,12 @@ initial begin
     #10
     a = 1; b = 1; cin = 1;
     #10 
+
+    /* for (i = 0; i < 16; i = i + 1) begin
+        {one, two} = i;
+        #10
+    end */
+
     $finish();
 end
 
