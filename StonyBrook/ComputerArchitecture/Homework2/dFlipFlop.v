6f6a6e8752bd7dbcdcd4462bdd97ca3b537895af
Niki Shah <email4nikhil@gmail.com>
2025-10-15 00:03:53 -0400

    fixed the flip flop

--- StonyBrook/ComputerArchitecture/Homework2/dFlipFlop.v
+++ StonyBrook/ComputerArchitecture/Homework2/dFlipFlop.v
@@ -1,4 +1,4 @@
-module dFlipFlop ( input clk, input d, output q, output qbar);
+module dFlipFlop ( input clk, input d, output reg q, output reg qbar);
     always @(posedge clk) begin
         q <= d;
         qbar <= !d;
