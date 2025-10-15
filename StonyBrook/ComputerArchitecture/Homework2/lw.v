6f23ef12d033bad4400b0f4ff1b2d5affd60d9bf
Nikhil <email4nikhil@gmail.com>
2025-10-15 13:54:40 -0400

    added the sign extend to lw and sw

--- StonyBrook/ComputerArchitecture/Homework2/lw.v
+++ StonyBrook/ComputerArchitecture/Homework2/lw.v
@@ -1,4 +1,7 @@
-module lw ( input [31:0] address, output [31:0] data, memoryFile mem);
+module lw ( input [31:0] address, input [15:0] offset, output [31:0] data, memoryFile mem);
     // Load word from address to data
-    assign data = mem.memory[address[7:0]];
+    wire [31:0] effective_address;
+    // sign extend offset
+    assign effective_address = {{16{offset[15]}}, offset} + address;
+    assign data = mem.memory[effective_address[7:0]];
 endmodule
--- StonyBrook/ComputerArchitecture/Homework2/sw.v
+++ StonyBrook/ComputerArchitecture/Homework2/sw.v
@@ -1,4 +1,10 @@
-module sw ( input [31:0] source, input offset, output [31:0] destination);
+module sw ( input [31:0] source, input offset, memoryFile mem, output [31:0] destination);
     // Save word from source to destination with offset
-    assign destination = source + offset;
+    // sign extend offset
+    wire [31:0] effective_offset;
+    assign effective_offset = {{16{offset[15]}}, offset};
+    assign destination = source + effective_offset;
+    always @(posedge clk) begin
+        mem.memory[destination[7:0]] <= source;
+    end
 endmodule
