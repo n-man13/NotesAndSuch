7f88b4a1d6d5f7469aa1ae49b135ac331795bd90
Nikhil <email4nikhil@gmail.com>
2025-10-18 15:22:14 -0400

    added testbench

--- StonyBrook/ComputerArchitecture/Homework2/memoryFile.v
+++ StonyBrook/ComputerArchitecture/Homework2/memoryFile.v
@@ -1,6 +1,15 @@
 module memoryFile ( input [31:0] address, input write_enable, input [31:0] write_data, output [31:0] read_data);
     // Memory file, just output address as data
     reg [31:0] memory [0:255];
+
+    initial begin
+        // Initialize memory to 0
+        integer i;
+        for (i = 0; i < 256; i = i + 1) begin
+            memory[i] = 32'b0;
+        end
+    end
+    
     always @(!write_enable) begin
         read_data <= memory[address[7:0]];
     end
--- StonyBrook/ComputerArchitecture/Homework2/registerFile.v
+++ StonyBrook/ComputerArchitecture/Homework2/registerFile.v
@@ -2,6 +2,15 @@ module registerFile ( input [4:0] readReg1, input [4:0] readReg2,
 input [4:0] writeReg, input [31:0] writeData, input writeEnable, 
 output [31:0] readData1, output [31:0] readData2);
     reg [31:0] registers [31:0];
+
+    intial begin
+        // Initialize registers to 0
+        integer i;
+        for (i = 0; i < 32; i = i + 1) begin
+            registers[i] = 32'b0;
+        end
+    end
+
     always @(*) begin
         if (writeEnable) begin
             registers[writeReg] <= writeData;
