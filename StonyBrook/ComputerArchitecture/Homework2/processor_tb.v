66022c7768a794fd9dc633a9e6fef82a9fd5c11b
Nikhil <email4nikhil@gmail.com>
2025-10-21 22:39:38 -0400

    started writing up the report

--- StonyBrook/ComputerArchitecture/Homework2/processor_tb.v
+++ StonyBrook/ComputerArchitecture/Homework2/processor_tb.v
@@ -2,19 +2,13 @@
 module processor_tb;
 
     wire clk;
-    wire [31:0] pc;
     initial begin
         $dumpfile("hw2.vcd");
         $dumpvars(0, processor_tb);
         //$stop;
-        pc = 0;
         #200; // Run simulation for 200 time units
-        pc = 8;
-        #200;
-        pc = 20;
-        #200;
-        $finish;
+        
     end
-    processor myProcessor(.initial_pc(pc));
+    processor myProcessor(.initial_pc(0));
 
 endmodule
